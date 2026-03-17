dc1908c93910a89acb513303ff421e76e215a429
raghav3207 <122003620+raghav3207@users.noreply.github.com>
2026-03-18 05:05:04 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
